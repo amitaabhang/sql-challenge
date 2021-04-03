59367ce27bc05ffb202c2a07b9cd521112be4c39
amita a <amita.abhang@gmail.com>
2021-04-03 00:59:03 -0400

    Homework_SQL_Checkin

---
+++ EmployeeSQL/SQL Queries/Query_7.sql
@@ -0,0 +1,15 @@
+--7. List all employees in the Sales and Development departments,
+--including their employee number, last name, first name, and department name.
+
+select
+E.emp_id,
+E.first_name,
+E.last_name,
+D.dept_name 
+from employees E
+JOIN department_employee DE
+on E.emp_id = DE.emp_no
+JOIN departments D 
+on DE.dept_num =D.dept_no
+where TRIM(D.dept_name) in ('Sales','Development')
+order by E.emp_id
